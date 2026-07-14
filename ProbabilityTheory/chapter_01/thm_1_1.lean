53da1cd328a00a7fc5f5b158d7251d66e55cf47a
Tuo Feng <kdsdengshuo2823@gmail.com>
2026-07-14 18:34:40 +0800

    fix(ch1): remove theorem 1.1 conflict markers

--- ProbabilityTheory/chapter_01/thm_1_1.lean
+++ ProbabilityTheory/chapter_01/thm_1_1.lean
@@ -2,10 +2,6 @@ import ProbabilityTheory.chapter_01.thm_1_1_common_limit
 import Mathlib.Topology.Order.ProjIcc
 
 open Finset BigOperators
-<<<<<<< HEAD
-open MeasureTheory
-=======
->>>>>>> 9f9e0899aab75b7bd64d9e5f546a93c80411d6af
 open Set Topology
 
 noncomputable section
